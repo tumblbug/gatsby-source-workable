db82870b88c9ac353df263f370c4540307e400a2
Chrish Dunne <hey@chrish.design>
2020-07-01 12:41:58 +0100

    Add bottleneck limiting

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,43 +1,32 @@
 const crypto = require("crypto");
 const axios = require("axios");
-const rateLimit = require("axios-rate-limit");
+const Bottleneck = require("bottleneck");
+
+const limiter = Bottleneck({
+  minTime: 100,
+});
 
 exports.sourceNodes = async (
   { boundActionCreators: { createNode } },
   { subdomain, apiKey, queryParams = { state: "published" }, fetchJobDetails }
 ) => {
-  const axiosClient = rateLimit(
-    axios.create({
-      baseURL: `https://${subdomain}.workable.com/spi/v3/`,
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    }),
-    {
-      maxRequests: 1,
-      perMilliseconds: 1000,
-    }
-  );
+  const axiosClient = axios.create({
+    baseURL: `https://${subdomain}.workable.com/spi/v3/`,
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  });
 
   // Get list of all jobs
   const {
     data: { jobs },
-  } = await axiosClient.get("/jobs", { params: queryParams });
+  } = limiter.schedule(() => axiosClient.get("/jobs", { params: queryParams }));
 
   for (const job of jobs) {
     // Fetch job details if needed
-    // const jobData = fetchJobDetails ? (
-    //   axiosClient.getMaxRPS();
-    //   await axiosClient.get(`/jobs/${job.shortcode}`)
-    // ).data: job;
-
-    let jobData;
-    if (fetchJobDetails) {
-      axiosClient.getMaxRPS();
-      jobData = await axiosClient.get(`/jobs/${job.shortcode}`);
-    } else {
-      jobData = job;
-    }
+    const jobData = fetchJobDetails
+      ? (await axiosClient.get(`/jobs/${job.shortcode}`)).data
+      : job;
 
     const jsonString = JSON.stringify(jobData);
     const gatsbyNode = {
