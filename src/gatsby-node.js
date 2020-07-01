737971d0c87e9247753cdcef07917aeb6de6ba3f
Chrish Dunne <hey@chrish.design>
2020-07-01 12:19:12 +0100

    Add another attempted fix

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,8 +1,11 @@
-const crypto = require('crypto')
-const axios = require('axios')
-const rateLimit = require('axios-rate-limit');
+const crypto = require("crypto");
+const axios = require("axios");
+const rateLimit = require("axios-rate-limit");
 
-exports.sourceNodes = async ({ boundActionCreators: { createNode } }, { subdomain, apiKey, queryParams = { state: 'published' }, fetchJobDetails }) => {
+exports.sourceNodes = async (
+  { boundActionCreators: { createNode } },
+  { subdomain, apiKey, queryParams = { state: "published" }, fetchJobDetails }
+) => {
   const axiosClient = rateLimit(
     axios.create({
       baseURL: `https://${subdomain}.workable.com/spi/v3/`,
@@ -11,30 +14,46 @@ exports.sourceNodes = async ({ boundActionCreators: { createNode } }, { subdomai
       },
     }),
     {
-      maxRPS: 10,
+      maxRequests: 1,
+      perMilliseconds: 1000,
     }
   );
 
   // Get list of all jobs
-  axiosClient.getMaxRPS();
-  const { data: { jobs } } = await axiosClient.get('/jobs', { params: queryParams });
+  const {
+    data: { jobs },
+  } = await axiosClient.get("/jobs", { params: queryParams });
 
-  for(const job of jobs) {
+  for (const job of jobs) {
     // Fetch job details if needed
-    const jobData = fetchJobDetails ? (await axiosClient.get(`/jobs/${job.shortcode}`)).data : job;
+    // const jobData = fetchJobDetails ? (
+    //   axiosClient.getMaxRPS();
+    //   await axiosClient.get(`/jobs/${job.shortcode}`)
+    // ).data: job;
 
-    const jsonString = JSON.stringify(jobData)
+    let jobData;
+    if (fetchJobDetails) {
+      axiosClient.getMaxRPS();
+      jobData = await axiosClient.get(`/jobs/${job.shortcode}`);
+    } else {
+      jobData = job;
+    }
+
+    const jsonString = JSON.stringify(jobData);
     const gatsbyNode = {
       ...jobData,
       children: [],
-      parent: '__SOURCE__',
+      parent: "__SOURCE__",
       internal: {
-        type: 'WorkableJob',
+        type: "WorkableJob",
         content: jsonString,
-        contentDigest: crypto.createHash('md5').update(jsonString).digest('hex'),
+        contentDigest: crypto
+          .createHash("md5")
+          .update(jsonString)
+          .digest("hex"),
       },
-    }
+    };
     // Insert data into gatsby
-    createNode(gatsbyNode)
+    createNode(gatsbyNode);
   }
-}
+};
