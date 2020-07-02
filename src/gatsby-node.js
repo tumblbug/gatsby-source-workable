72914b441607bcd9f928e3cafbf15f0d467ceb04
Chrish Dunne <hey@chrish.design>
2020-07-02 11:28:06 +0100

    Another attempt at rate limiting

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,26 +1,31 @@
 const crypto = require("crypto");
 const axios = require("axios");
-const Bottleneck = require("bottleneck");
-
-const limiter = Bottleneck({
-  minTime: 100,
-});
+const LimitRate = require("axios-rate-limit");
 
 exports.sourceNodes = async (
   { boundActionCreators: { createNode } },
   { subdomain, apiKey, queryParams = { state: "published" }, fetchJobDetails }
 ) => {
-  const axiosClient = axios.create({
-    baseURL: `https://${subdomain}.workable.com/spi/v3/`,
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-    },
-  });
+  function CreateRateLimitedAxios() {
+    const axiosInstance = axios.create({
+      baseURL: `https://${subdomain}.workable.com/spi/v3/`,
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+
+    return LimitRate(axiosInstance, {
+      maxRequests: 10,
+      perMilliseconds: 10000,
+    });
+  }
+
+  const axiosClient = CreateRateLimitedAxios();
 
   // Get list of all jobs
   const {
     data: { jobs },
-  } = limiter.schedule(() => axiosClient.get("/jobs", { params: queryParams }));
+  } = axiosClient.get("/jobs", { params: queryParams });
 
   for (const job of jobs) {
     // Fetch job details if needed
